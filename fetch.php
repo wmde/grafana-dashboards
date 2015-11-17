5f2a3d2f73fe4501eeae6e473b9e26bd931dfb2d
addshore <addshorewiki@gmail.com>
2015-11-17 16:35:13 +0100

    Add simple script for fetching grafana dashboards

---
+++ fetch.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Addshore
+ * Quick script for grabbing a list of dashboards
+ */
+
+$dashboards = array(
+	'wikidata',
+	'wikidata-api-wbgetclaims',
+	'wikidata-dispatch',
+	'wikidata-labs-builder',
+	'wikidata-page-views-per-domain',
+	'wikidata-social-followers',
+);
+
+foreach( $dashboards as $dashboard ) {
+	echo "Getting dashboard $dashboard\n"; 
+	$response = file_get_contents( "https://grafana.wikimedia.org/api/dashboards/db/$dashboard" );
+	$data = json_decode( $response, true );
+	$data = $data['dashboard'];
+	$json = json_encode( $data, JSON_PRETTY_PRINT );
+	file_put_contents( __DIR__ . '/' . $dashboard . ".json", $json );
+}
+
+echo "Done";
